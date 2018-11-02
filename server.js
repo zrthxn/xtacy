8f32b58351a79a486269b87b38f53e5c440ee332
Alisamar Husain <35369637+zarath1810@users.noreply.github.com>
2018-11-03 00:15:45 +0530

    Server

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+/**
+ * @author Alisamar Husain
+ * Server for the fest website
+ */
+
 const express = require('express');
 const hbs = require('express-handlebars');
 const path = require('path');
@@ -5,11 +10,25 @@ const homepage = express();
 
 const PORT = process.env.PORT || 3000;
 
-homepage.use( express.static(path.join(__dirname, 'homepage')) );
-homepage.get('/', (req, res)=>{
-    res.sendFile( path.resolve(__dirname, 'homepage', 'index.html') );
-});
-
 homepage.listen(PORT, ()=>{
     console.log("SERVER :: Initialized :: Listening on PORT : ", PORT);
+});
+
+homepage.use( express.static(path.join(__dirname, 'homepage')) );
+
+homepage.set('views', path.join(__dirname, 'homepage'));
+homepage.set('view engine', 'hbs');
+homepage.engine('hbs', handle({
+    defaultLayout: 'main',
+    extname: 'hbs',
+    layoutsDir: __dirname + '/homepage/layouts',
+    partialsDir  : [
+        __dirname + '/homepage/partials',
+    ]
+}));
+
+// ROUTER ===========================
+
+homepage.get('/', (req, res)=>{
+    res.sendFile( path.resolve(__dirname, 'homepage', 'index.html') );
 });
