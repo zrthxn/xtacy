5d2438247dd3abb163789e936dfaea21c7679cc7
Alisamar Husain <zrthxn@gmail.com>
2019-01-30 16:17:22 +0530

    Rollback Point

--- bookings/build/precache-manifest.8c42984bd17032f52aa98ed4494cbda1.js
+++ bookings/build/precache-manifest.8c42984bd17032f52aa98ed4494cbda1.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/book/start/static/js/runtime~main.7df28536.js"
   },
   {
-    "revision": "3002d8f7d59a8e2776be",
-    "url": "/book/start/static/js/main.3002d8f7.chunk.js"
+    "revision": "be6a5cc6dd56452a11df",
+    "url": "/book/start/static/js/main.be6a5cc6.chunk.js"
   },
   {
     "revision": "bb91790fda603e09e8b7",
     "url": "/book/start/static/js/1.bb91790f.chunk.js"
   },
   {
-    "revision": "3002d8f7d59a8e2776be",
+    "revision": "be6a5cc6dd56452a11df",
     "url": "/book/start/static/css/main.6a2bb779.chunk.css"
   },
   {
-    "revision": "422c434c84b024d5442dfee51098916e",
+    "revision": "288153babb08da169ae295f3c55547ad",
     "url": "/book/start/index.html"
   }
 ];
