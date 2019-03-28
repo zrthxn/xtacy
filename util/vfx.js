f9f86bec42c882c978d3e5f179fc2169adcbc2fe
shamikh <shamikh2000@gmail.com>
2019-03-28 21:15:29 +0530

    vfx

--- util/vfx.js
+++ util/vfx.js
@@ -2,10 +2,10 @@ const Database = require('./Database').firestore
 const EventManager = require('./EventManager')
 
 exports.verify = (ack) => {
-    Database.collection('acknowledgments').doc(ack.toString()).get().then((snapshot) => {
+    Database.collection('acknowledgments').doc(ack).get().then((snapshot) => {
         let dbData = snapshot.data()
         EventManager.generalRegister(dbData).then(() =>{
-            Database.collection('acknowledgments').doc(ack.toString()).delete().then(() => {
+            Database.collection('acknowledgments').doc(ack).delete().then(() => {
                 console.log("REGISTRATION VERFIED")
             })
         })
